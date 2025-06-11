334a2eafbc22873a69bf33c0dac051ff7aa8742f
Bouchet <victoria.bouchet6@gmail.com>
2025-06-11 15:31:28 +0200

    Ajustement et correction des erreurs de 2opt

--- 2opt.py
+++ 2opt.py
@@ -1,36 +1,38 @@
 #Code permettant de résoudre le TSP avec la méthode 2-opt
 #Se basant sur des 2-permutations
 
-def path_lenght_matrix(path):
+def path_length_matrix(path, dist_matrix):
     """
-    Fonction permettant le calcul de la longueur du chemin initial
+    Calcule la longueur totale d'un chemin donné en fonction d'une matrice de distance
     """
     tot = 0                                #Valeur initialisée à 0
     for i in range(len(path)):
         first = path[i]
         second = path[(i+1)%len(path)]     #Format = (i+1) mod longueur du chemin car parcours cyclique
-        tot += build_matrix[first][second] #Ajout à la matrice des distances
+        tot += dist_matrix[first][second]  #Ajout de la distance entre deux villes au total
     return tot 
 
-def two_opt(index, dist_matrix):
+def two_opt(path, dist_matrix):
     """
-    METHODE 2-OPT : Implémentation d'une heuristique afin de résoudre le TSP
+    METHODE 2-OPT : Amélioration d'un chemin du TSP
+    Prend en entrée un chemin (une liste d'indices) et une matrice de distance
+    Renvoie un chemin amélioré à l'aide de 2-permutations (inversion d'arête)
     """
-    n = len(index)
+    n = len(path)
     improve = True #Initialisation du booléen d'amélioration à Vrai
     
     while improve:
         improve = False
     
         for i in range(n-1):
-                for j in range(i+2, n if i>0 else n-1):     #Boucle sur les paires d'arêtes
-                    a, b = index[i], index[i+1]             # a --> b
-                    c, d = index[j], index[(j+1)%n]         # c --> d
+                for j in range(i+2, n if i>0 else n-1):   #Empêche les inversions invalides
+                    a, b = path[i], path[i+1]             # a --> b
+                    c, d = path[j], path[(j+1)%n]         # c --> d
                     
                     #Calcul du gain de distance
                     delta = -dist_matrix[a][b] -dist_matrix[c][d] + dist_matrix[a][c] + dist_matrix[b][d]
 
                     if delta < 0: #Si delta <0 alors le chemin est plus court, on procède à l'échange
-                        index[i+1:j+1] = reversed(index[i+1, j+1])
+                        path[i+1:j+1] = reversed(path[i+1:j+1])
                         improve = True
-    return index
+    return path
