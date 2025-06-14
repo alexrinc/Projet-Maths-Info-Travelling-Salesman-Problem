f2bde24c14ff3506f69c4ed07a17f59155db8b06
alexrinc <microalex@outlook.fr>
2025-06-15 00:50:14 +0200

    Ajout de Simulated Annealing + Màj de main

---
+++ algorithms/annealing.py
@@ -0,0 +1,65 @@
+import math
+import random
+
+def total_distance(route, coords):
+    dist = 0
+    for i in range(len(route)):
+        ville1 = coords[route[i]]
+        ville2 = coords[route[(i + 1) % len(route)]]
+        dist += math.sqrt((ville1[0] - ville2[0]) ** 2 + (ville1[1] - ville2[1]) ** 2)
+    return dist
+
+def simulated_annealing(coords, path, initial_temp, cooling_rate):   
+    current_path = path
+    current_distance = total_distance(current_path, coords)
+
+    best_path = current_path[:]
+    best_distance = current_distance
+
+    T = initial_temp
+    max_iterations = 10000
+
+    for iteration in range(max_iterations):
+        # 2-opt
+        i = random.randint(0, len(coords) - 2)
+        k = random.randint(i + 1, len(coords) - 1)
+        
+        new_path = current_path[:i] + current_path[i:k+1][::-1] + current_path[k+1:]
+        new_distance = total_distance(new_path, coords)
+        delta = new_distance - current_distance
+        
+        # Est-ce une meilleure solution ?
+        if delta < 0 or random.random() < math.exp(-delta / T):
+            current_path = new_path
+            current_distance = new_distance
+            # Mise à jour de la meilleure solution
+            if new_distance < best_distance:
+                best_path = new_path
+                best_distance = new_distance
+
+        # Réduit la température.
+        T *= cooling_rate
+        
+        """ if iteration % 1000 == 0: # Pour suivre l'avancée
+            print(f"Iteration {iteration}: distance actuelle = {current_distance:.4f}, meilleure distance = {best_distance:.4f}") """
+
+    index = best_path.index(0)                          # L'index de la ville 0
+    best_path = best_path[index:] + best_path[:index]   # On réarrange le chemin pour commencer par la ville 0 (le chemin reste le même)
+    best_path.append(0)                                 # Retour à la ville de départ (0)
+    return best_path, best_distance
+
+if __name__ == '__main__': # TEST
+    nb_villes = 10
+    cities = [(random.uniform(0, 100), random.uniform(0, 100)) for _ in range(nb_villes)]
+    
+    # Haute température de départ pour une exploration large des solutions, refroidissement lent pour une convergence graduelle
+    initial_temp = 10000
+    cooling_rate = 0.995
+    
+    path = [0, 2, 9, 3, 7, 6, 5, 1, 4, 8]
+    
+    best_path, best_distance = simulated_annealing(cities, path, initial_temp, cooling_rate)
+    
+    print("\nBest path :")
+    print(best_path)
+    print(f"Total distance: {best_distance:.4f}")
--- main.py
+++ main.py
@@ -7,10 +7,11 @@ import functions.generate as generate
 import algorithms.neighbor as neighbor
 import algorithms.insertion as insertion
 import algorithms.twoopt as twoopt
+import algorithms.annealing as annealing
 import functions.visualisation as vs
 
 ### MAIN ###
-distance_matrix, coords = generate.distance_matrix(50)
+distance_matrix, coords = generate.distance_matrix(10)
 
 print("Matrice des distances entre les villes: \n")
 print(distance_matrix)
@@ -25,6 +26,13 @@ print(f'Route la plus courte pour l\'algorithme " Insertion " : \n{algo2}')
 #Choix du chemin de Nearest Neighbor pour le calcul de cette heuristique
 algo3 = twoopt.two_opt(algo1.copy(), distance_matrix)
 print(f'Route la plus courte pour l\'algorithme " Two Opt " : \n{algo3}')
+
+initial_temperature = 10000
+cooling_rate = 0.995
+path = algo1.copy()
+path.pop() # On enlève le dernier 0 de Nearest Neighbor pour éviter les doublons
+algo4_tour, algo4_length = annealing.simulated_annealing(coords, path, initial_temperature, cooling_rate)
+print(f'Route la plus courte pour l\'algorithme " Simulated Annealing " : \n{algo4_tour}')
 
 #Affichage sur la même figure des visualisations des 3 heuristiques
 vs.plot_path(coords, [algo1, algo2, algo3], ["Nearest Neighbor", "Insertion", "Two Opt"])
