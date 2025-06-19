19a58def25e82a056031915fd126e10837c58f10
Bouchet <victoria.bouchet6@gmail.com>
2025-06-19 13:59:53 +0200

    Correction de l'orthographe des commentaires

--- algorithms/insertion.py
+++ algorithms/insertion.py
@@ -1,4 +1,3 @@
-#Code de l'heuristique d'insertion minimale
 import functions.distance as distance
 
 def cheapest_insertion(distance_matrix):
@@ -11,29 +10,29 @@ def cheapest_insertion(distance_matrix):
 
     """
     n = len(distance_matrix)                             #Nombre total des villes
-    unvisited = list(range(n))                           #Liste des idices des villes non inserees dans le circuit
+    unvisited = list(range(n))                           #Liste des indices des villes non inserees dans le circuit
     path = [unvisited.pop(0), unvisited.pop(0)]          #Initialisation du chemin avec les deux premières villes
 
 
     while unvisited:
-        #Recherhce la meilleure ville à inserer et de sa position optimale
+        #Recherche la meilleure ville à inserer et de sa position optimale
 
-        best_cost = float('inf')                            #Init avec la valeur infini pour technique borne inférieur
+        best_cost = float('inf')                            #Init avec la valeur infini pour la technique de borne inférieure
         best_place = None
-        to_insert = None                                    #Ville à insérer lorsque celle-ci augmente le moins le coût total du chemin
+        to_insert = None                                    #Ville à insérer lorsque celle-ci a la plus faible augmentation du coût total
 
         for v in unvisited:                                 #Insertion de chaque ville restantes une à une
             for i in range(len(path)):
-                test_path = path[:i+1] + [v] + path[i+1:]   #Création d'un nouveau chemin hypothétique ave la ville v
+                test_path = path[:i+1] + [v] + path[i+1:]   #Création d'un nouveau chemin hypothétique avec la ville v
                 new_cost = distance.total_path_distance(test_path, distance_matrix)
 
-                #Si l'insertion donne un meilleur cout alors on la garde en mémoire comme meilleure option
+                #Si l'insertion donne un cout plus optimal alors on la garde en mémoire comme meilleure option
                 if new_cost < best_cost:
                     best_cost = new_cost    
                     best_place = i+1
                     to_insert = v
 
-        #Insertion de la meilleure ville à la meilleure position
+        #Insertion de la "meilleure ville" à la "meilleure position"
         path.insert(best_place, to_insert)
         unvisited.remove(to_insert)
 
