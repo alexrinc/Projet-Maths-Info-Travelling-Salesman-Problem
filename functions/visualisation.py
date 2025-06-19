68746119ce64c03cd61dd2a28a3c217ed94283f3
Bouchet <victoria.bouchet6@gmail.com>
2025-06-19 11:35:31 +0200

    Ajout de commentaires sur visualisation

--- functions/visualisation.py
+++ functions/visualisation.py
@@ -10,7 +10,7 @@ def plot_path(coord, paths, names):
     """
     fig, axis = plt.subplots(1, len(paths), figsize=(5 * len(paths), 6))
     
-    if len(paths) == 1:
+    if len(paths) == 1: #Si un seul chemin: objet unique, non une liste
         axis = [axis]
 
     for ax, path, name in zip(axis, paths, names):
@@ -21,20 +21,20 @@ def plot_path(coord, paths, names):
         
         # --- VISUALISATION DU CHEMIN OBTENU ---
         #plt.figure(figsize=(8,6))
-        ax.plot(x_path, y_path, 'ro-', label ='Chemin TSP', markersize=5)  #r --> rouge ; o --> marqueur cercle ; "-" --> relié par des lignes 
+        ax.plot(x_path, y_path, 'ro-', label ='Chemin TSP', markersize=5) #r --> rouge ; o --> marqueur cercle ; "-" --> relié par des lignes 
         
-        # --- AFFICHAGE DES NUMEROS DE VILLES ---
+        # --- AFFICHAGE DES NUMEROS DE VILLES --
         # On parcourt les coordonnées originales pour placer chaque numéros
         for i, (x,y) in enumerate(coord):
             #Affiche le numéro de la ville 'i' (on décale un peu pour la lisibilité)
-            ax.text(x,y + 0.05, str(i), color='blue', fontsize=12, ha='center')
+            ax.text(x,y + 0.05, str(i), color='blue', fontsize=12, ha='center') 
 
         # Mise en évidence du point de départ
         start_city_coords = coord[path[0]]
-        ax.plot(start_city_coords[0], start_city_coords[1], 'go', markersize=10, label="Ville de départ")
+        ax.plot(start_city_coords[0], start_city_coords[1], 'go', markersize=10, label="Ville de départ") #g --> vert ; o --> marqueur cercle
 
 
-        # --- Mise en forme du graphique ---
+        # --- MISE EN FORME DU GRAPHIQUE ---
         ax.set_title(f"Méthode {name}")
         ax.set_xlabel("Coordonnée x")       #Axe des x
         ax.set_ylabel("Coordonnée y")       #Axe des y
